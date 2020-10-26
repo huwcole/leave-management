1a8b012aa05bc4f2c8275742cceefe1b568755ed
uwe kohl <uwe.kohl@refer-all.net>
2020-10-26 23:17:11 +0000

    update

--- leave-management/Contracts/IRepositoryBase.cs
+++ leave-management/Contracts/IRepositoryBase.cs
@@ -1,18 +1,19 @@
-﻿using leave_management.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace leave_management.Contracts
 {
     public interface IRepositoryBase<T> where T : class
     {
         ICollection<T> FindAll();
+
         T FindById(int id);
+
         bool Create(T entity);
+
         bool Update(T entity);
+
         bool Delete(T entity);
+
         bool Save();
     }
 }
