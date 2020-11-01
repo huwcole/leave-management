f87f988cc19effcf120724d7075e9536e8e9aaa7
uwe kohl <uwe.kohl@refer-all.net>
2020-11-01 12:21:22 +0000

    update

--- leave-management/Models/EmployeeVM.cs
+++ leave-management/Models/EmployeeVM.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace leave_management.Models
 {
@@ -10,16 +11,22 @@ namespace leave_management.Models
 
         public string Email { get; set; }
 
+        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
 
+        [Display(Name = "First name")]
         public string Firstname { get; set; }
 
+        [Display(Name = "Last name")]
         public string Lastname { get; set; }
 
+        [Display(Name = "Tax ID")]
         public string TaxId { get; set; }
 
+        [Display(Name = "Date of birth")]
         public DateTime DateOfBirth { get; set; }
 
+        [Display(Name = "Date joined")]
         public DateTime DateJoined { get; set; }
     }
 }
